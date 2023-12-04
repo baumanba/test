6c05a37d880f8709b966c1a8e344dc78e4077a40
baumanba <152903002+baumanba@users.noreply.github.com>
2023-12-04 18:32:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-//Initialize the map
-var map = L.map('map').setView([0, 0], 2);
+<script type="text/javascript">
+  // Initialize the map
+  var map = L.map('map').setView([40.04, -76.31], 10);
 
-//Add OpenStreetMap as the base layer
-L.tileLayer('https{s}.tile.openstreetmap.org{z}{x}{y}.png', {
-    attribution '© OpenStreetMap contributors'
-}).addTo(map);
+  // Add OpenStreetMap as the base layer
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors'
+  }).addTo(map);
 
-//Load GeoJSON data and add it to the map
-fetch('"C:\Users\farglenuts\Desktop\test git\data.json.txt"')
-    .then(response = response.json())
-    .then(data = {
-        L.geoJSON(data).addTo(map);
-    });
+  // Load GeoJSON data and add features to the map
+  fetch('tester.geojson') // Assuming your file is named tester.geojson
+      .then(response = response.json())
+      .then(data = {
+          // Create a GeoJSON layer with all features
+          var geoJsonLayer = L.geoJSON(data).addTo(map);
+      })
+      .catch(error = {
+          console.error('Error loading GeoJSON:', error);
+      });
+</script>
